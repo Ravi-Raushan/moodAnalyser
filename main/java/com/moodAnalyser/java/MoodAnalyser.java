ec492f125f924da07f83c0808f6c4de19496149c
Ravi-Raushan <krravi015@gmail.com>
2020-03-27 22:31:06 +0530

    [Ravi Raushan]Refactor. MoodAnalyser class to take the mood message in constructor

--- main/java/com/moodAnalyser/java/MoodAnalyser.java
+++ main/java/com/moodAnalyser/java/MoodAnalyser.java
@@ -1,7 +1,14 @@
 package com.moodAnalyser.java;
 
 public class MoodAnalyser {
-    public String analyseMood(String message) {
+    String message;
+    public  MoodAnalyser(){
+
+    }
+    public MoodAnalyser(String message){
+        this.message=message;
+    }
+    public String analyseMood() {
             if (message.contains("sad"))
                 return "SAD";
             return "HAPPY";
